96dcc42f946c0d23563bdeb1ee91d8697bd2e154
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2025-06-07 20:48:34 +1200

    refactor: Update nodemon ignore list and remove start.js entry point

--- start.js
+++
@@ -1,9 +0,0 @@
-// start.js
-// Entry point to start the Express server for the Focal Point application.
-const app = require('./server');
-const PORT = process.env.PORT || 3000;
-
-// Start the server and log the URL
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
